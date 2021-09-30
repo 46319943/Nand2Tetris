35e6d5e5b8147d8708870406809d6966e62914f2
46319943 <495384481@qq.com>
2021-09-30 11:05:56 +0800

    完成VMTranslator

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,16 +1,45 @@
 import sys
 import os
-from vm_translator import parse
+import glob
+from vm_translator import parse, booting
 
 if __name__ == '__main__':
-    file_name = sys.argv[1]
+    input_path = sys.argv[1]
 
-    f = open(file_name)
-    lines = f.readlines()
-    f.close()
+    if os.path.isdir(input_path):
+        dir_path = input_path
+        dir_name = os.path.basename(dir_path)
 
-    lines_parsed = parse(lines, os.path.basename(file_name).rstrip('.vm'))
+        file_path_list = [file_path for file_path in glob.glob(dir_path + '/*.vm')]
+        lines_parsed_total = booting()
 
-    f = open(file_name.replace('.vm', '.asm'), 'w')
-    f.write('\n'.join(lines_parsed))
-    f.close()
+        for file_path in file_path_list:
+            file_name = os.path.basename(file_path)
+            file_base_name = file_name.rstrip('.vm')
+
+            f = open(file_path)
+            lines = f.readlines()
+            f.close()
+
+            lines_parsed = parse(lines, file_base_name)
+            lines_parsed_total.extend(lines_parsed)
+
+        f = open(os.path.join(dir_path, dir_name + '.asm'), 'w')
+        f.write('\n'.join(lines_parsed_total))
+        f.close()
+
+    else:
+        file_path = input_path
+
+        file_name = os.path.basename(file_path)
+        file_base_name = file_name.rstrip('.vm')
+
+        f = open(file_path)
+        lines = f.readlines()
+        f.close()
+
+        lines_parsed = parse(lines, file_base_name)
+
+        f = open(file_path.replace('.vm', '.asm'), 'w')
+        f.write('\n'.join(lines_parsed))
+        f.close()
