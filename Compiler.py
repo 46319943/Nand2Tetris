765c14b5fedca4d1d621b673796bc0a7d55149c4
46319943 <495384481@qq.com>
2021-10-06 15:37:30 +0800

    syntax_analyzer init

---
+++ Compiler.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import glob
+from syntax_analyzer import tokenize
+
+if __name__ == '__main__':
+    input_path = sys.argv[1]
+
+    if os.path.isdir(input_path):
+        dir_path = input_path
+        dir_name = os.path.basename(dir_path)
+
+        file_path_list = [file_path for file_path in glob.glob(dir_path + '/*.jack')]
+
+        for file_path in file_path_list:
+            file_name = os.path.basename(file_path)
+            file_base_name = file_name.rstrip('.jack')
+
+            f = open(file_path)
+            lines = f.readlines()
+            f.close()
+
+            ele_tree = tokenize(lines)
+            ele_tree.write(
+                file_path.replace('.jack', 'TT.xml')
+            )
+
+    else:
+        file_path = input_path
+
+        file_name = os.path.basename(file_path)
+        file_base_name = file_name.rstrip('.jack')
+
+        f = open(file_path)
+        lines = f.readlines()
+        f.close()
+
+        ele_tree = tokenize(lines)
+
+        ele_tree.write(file_path.replace('.jack', 'TT.xml'))
